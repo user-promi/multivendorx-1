2d152a17b9312d30566657b41019421fdc4a1170
user-promi <promita@dualcube.com>
2025-06-26 15:51:44 +0530

    Update Courses.tsx

--- plugins/moowoodle/src/components/Courses/Courses.tsx
+++ plugins/moowoodle/src/components/Courses/Courses.tsx
@@ -538,7 +538,7 @@ const Course: React.FC = () => {
                     aria-labelledby="form-dialog-title"
                 >
                     <span
-                        className="admin-font adminlib-cross stock-manager-popup-cross"
+                        className="admin-font adminlib-cross"
                         onClick={() => setOpenDialog(false)}
                     ></span>
                     <ProPopup />
