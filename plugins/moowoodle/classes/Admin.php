99f1e32b22618055c8a0968fc442a7f0c0aab334
user-promi <promita@dualcube.com>
2025-06-30 17:11:15 +0530

    Update Admin.php

--- plugins/moowoodle/classes/Admin.php
+++ plugins/moowoodle/classes/Admin.php
@@ -47,81 +47,74 @@ class Admin {
                 50
 		    );
 
-			self::add_submenu();
-        }
-    }
+			$pro_sticker = ! Util::is_khali_dabba() ?
 
-	/**
-	 * Add Option page.
-	 */
-	public static function add_submenu() {
-		$pro_sticker = ! Util::is_khali_dabba() ?
+			'<span class="mw-pro-tag" style="font-size: 0.5rem; background: #e35047; padding: 0.125rem 0.5rem; color: #F9F8FB; font-weight: 700; line-height: 1.1; position: absolute; border-radius: 2rem 0; right: -0.75rem; top: 50%; transform: translateY(-50%)">Pro</span>' : '';
 
-		'<span class="mw-pro-tag" style="font-size: 0.5rem; background: #e35047; padding: 0.125rem 0.5rem; color: #F9F8FB; font-weight: 700; line-height: 1.1; position: absolute; border-radius: 2rem 0; right: -0.75rem; top: 50%; transform: translateY(-50%)">Pro</span>' : '';
+			// Array contain moowoodle submenu.
+			$submenus = array(
+				'courses'         => array(
+					'name'   => __( 'Courses', 'moowoodle' ),
+					'subtab' => '',
+				),
+				'cohorts'         => array(
+					'name'   => __( 'Cohorts', 'moowoodle' ) . $pro_sticker,
+					'subtab' => '',
+				),
+				'enrolments'      => array(
+					'name'   => __( 'Enrolments', 'moowoodle' ) . $pro_sticker,
+					'subtab' => '',
+				),
+				'settings'        => array(
+					'name'   => __( 'Settings', 'moowoodle' ),
+					'subtab' => 'general',
+				),
+				'synchronization' => array(
+					'name'   => __( 'Synchronization', 'moowoodle' ),
+					'subtab' => 'synchronize-course',
+				),
+			);
 
-		// Array contain moowoodle submenu.
-		$submenus = array(
-			'courses'         => array(
-				'name'   => __( 'Courses', 'moowoodle' ),
-				'subtab' => '',
-			),
-			'cohorts'         => array(
-				'name'   => __( 'Cohorts', 'moowoodle' ) . $pro_sticker,
-				'subtab' => '',
-			),
-			'enrolments'      => array(
-				'name'   => __( 'Enrolments', 'moowoodle' ) . $pro_sticker,
-				'subtab' => '',
-			),
-			'settings'        => array(
-				'name'   => __( 'Settings', 'moowoodle' ),
-				'subtab' => 'general',
-			),
-			'synchronization' => array(
-				'name'   => __( 'Synchronization', 'moowoodle' ),
-				'subtab' => 'synchronize-course',
-			),
-		);
+			// Register all submenu.
+			foreach ( $submenus as $slug => $submenu ) {
+				// prepare subtab if subtab is exist.
+				$subtab = '';
 
-		// Register all submenu.
-		foreach ( $submenus as $slug => $submenu ) {
-			// prepare subtab if subtab is exist.
-			$subtab = '';
+				if ( $submenu['subtab'] ) {
+					$subtab = '&subtab=' . $submenu['subtab'];
+				}
 
-			if ( $submenu['subtab'] ) {
-				$subtab = '&subtab=' . $submenu['subtab'];
+				add_submenu_page(
+					'moowoodle',
+					$submenu['name'],
+					"<span style='position: relative; display: block; width: 100%;' class='admin-menu'>" . $submenu['name'] . '</span>',
+					'manage_options',
+					'moowoodle#&tab=' . $slug . $subtab,
+					'_-return_null'
+				);
 			}
 
-			add_submenu_page(
-				'moowoodle',
-				$submenu['name'],
-                "<span style='position: relative; display: block; width: 100%;' class='admin-menu'>" . $submenu['name'] . '</span>',
-				'manage_options',
-				'moowoodle#&tab=' . $slug . $subtab,
-				'_-return_null'
-			);
-		}
+			// Register upgrade to pro submenu page.
+			if ( ! Util::is_khali_dabba() ) {
+				add_submenu_page(
+					'moowoodle',
+					__( 'Upgrade to Pro', 'moowoodle' ),
+					'<style>
+						a:has(.upgrade-to-pro){
+							background: linear-gradient(-28deg, #f6a091, #bb939c, #5f6eb3) !important;
+							color: White !important;
+						};
+					</style>
+					<div class="upgrade-to-pro"><i class="dashicons dashicons-awards"></i>' . esc_html__( 'Upgrade to Pro', 'moowoodle' ) . '</div> ',
+					'manage_options',
+					'',
+					array( self::class, 'handle_external_redirects' )
+				);
+			}
 
-		// Register upgrade to pro submenu page.
-		if ( ! Util::is_khali_dabba() ) {
-			add_submenu_page(
-				'moowoodle',
-				__( 'Upgrade to Pro', 'moowoodle' ),
-				'<style>
-					a:has(.upgrade-to-pro){
-						background: linear-gradient(-28deg, #f6a091, #bb939c, #5f6eb3) !important;
-						color: White !important;
-					};
-				</style>
-				<div class="upgrade-to-pro"><i class="dashicons dashicons-awards"></i>' . esc_html__( 'Upgrade to Pro', 'moowoodle' ) . '</div> ',
-				'manage_options',
-				'',
-				array( $this, 'handle_external_redirects' )
-			);
+			remove_submenu_page( 'moowoodle', 'moowoodle' );
 		}
-
-		remove_submenu_page( 'moowoodle', 'moowoodle' );
-	}
+    }
 
 	/**
      * Enqueue JavaScript for admin fronend page and localize script.
