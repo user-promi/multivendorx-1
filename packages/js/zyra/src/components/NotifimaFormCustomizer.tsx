85b6a1777c608db49cfa5c0631b6bcf6ad31307c
user-promi <promita@dualcube.com>
2025-07-01 16:23:44 +0530

    buddo fix

--- packages/js/zyra/src/components/NotifimaFormCustomizer.tsx
+++ packages/js/zyra/src/components/NotifimaFormCustomizer.tsx
@@ -48,113 +48,111 @@ const NotifimaFormCustomizer: React.FC<FormCustomizerProps> = ({
     }, []);
 
     return (
-        <div className="formcustomizer-wrapper">
-            <div className="wrapper-content">
-                <div className="label-section">
+        <div className="notifima-form-customizer">
+            <div className="label-section">
+                <BasicInput
+                    wrapperClass=""
+                    inputClass={
+                        currentHoverOn === 'description' ? 'active' : ''
+                    }
+                    ref={currentHoverOn === 'description' ? inputRef : null}
+                    value={setting?.alert_text || ''}
+                    onClick={() => setCurrentHoverOn('description')}
+                    onChange={(e) => onChange('alert_text', e.target.value)}
+                />
+            </div>
+
+            <div className="form-section">
+                <div
+                    ref={
+                        currentHoverOn === 'email_input' ? buttonRef : null
+                    }
+                    className="input-section"
+                >
                     <BasicInput
-                        wrapperClass=""
+                        type="email"
+                        wrapperClass="email-input-wrapper"
+                        readOnly
                         inputClass={
-                            currentHoverOn === 'description' ? 'active' : ''
+                            currentHoverOn === 'email_input' ? 'active' : ''
                         }
-                        ref={currentHoverOn === 'description' ? inputRef : null}
-                        value={setting?.alert_text || ''}
-                        onClick={() => setCurrentHoverOn('description')}
-                        onChange={(e) => onChange('alert_text', e.target.value)}
+                        placeholder={setting?.email_placeholder_text || ''}
+                        onClick={() => setCurrentHoverOn('email_input')}
                     />
-                </div>
 
-                <div className="form-section">
-                    <div
-                        ref={
-                            currentHoverOn === 'email_input' ? buttonRef : null
-                        }
-                        className="input-section"
-                    >
-                        <BasicInput
-                            type="email"
-                            readOnly
-                            inputClass={
-                                currentHoverOn === 'email_input' ? 'active' : ''
-                            }
-                            placeholder={setting?.email_placeholder_text || ''}
-                            onClick={() => setCurrentHoverOn('email_input')}
-                        />
+                    {currentHoverOn === 'email_input' && (
+                        <div
+                            className="input-editor"
+                            role="button"
+                            tabIndex={0}
+                            onClick={() => setCurrentEditSection('text')}
+                        >
+                            <p>Edit</p>
+                            <span>
+                                <i className="admin-font adminlib-edit"></i>
+                            </span>
+                        </div>
+                    )}
 
-                        {currentHoverOn === 'email_input' && (
-                            <div
-                                className="input-editor"
-                                role="button"
-                                tabIndex={0}
-                                onClick={() => setCurrentEditSection('text')}
-                            >
-                                <p>Edit</p>
-                                <span>
-                                    <i className="admin-font adminlib-edit"></i>
-                                </span>
-                            </div>
-                        )}
+                    {currentHoverOn === 'email_input' &&
+                        currentEditSection === 'text' && (
+                            <div className="setting-wrapper">
+                                <button
+                                    onClick={(e) => {
+                                        e.preventDefault();
+                                        setCurrentEditSection('');
+                                    }}
+                                    className="wrapper-close"
+                                >
+                                    <i className="admin-font adminlib-cross"></i>
+                                </button>
 
-                        {currentHoverOn === 'email_input' &&
-                            currentEditSection === 'text' && (
-                                <div className="setting-wrapper">
-                                    <div className="setting-nav">...</div>
-                                    <button
-                                        onClick={(e) => {
-                                            e.preventDefault();
-                                            setCurrentEditSection('');
-                                        }}
-                                        className="wrapper-close"
-                                    >
-                                        <i className="admin-font adminlib-cross"></i>
-                                    </button>
-
-                                    <div className="setting-section-dev">
-                                        <span className="label">
-                                            Placeholder text
-                                        </span>
-                                        <div className="property-section">
-                                            <BasicInput
-                                                type="text"
-                                                value={
-                                                    setting?.email_placeholder_text ||
-                                                    ''
-                                                }
-                                                onChange={(e) =>
-                                                    onChange(
-                                                        'email_placeholder_text',
-                                                        e.target.value
-                                                    )
-                                                }
-                                            />
-                                        </div>
+                                <div className="setting-section">
+                                    <span className="label">
+                                        Placeholder text
+                                    </span>
+                                    <div className="property-section">
+                                        <BasicInput
+                                            type="text"
+                                            value={
+                                                setting?.email_placeholder_text ||
+                                                ''
+                                            }
+                                            onChange={(e) =>
+                                                onChange(
+                                                    'email_placeholder_text',
+                                                    e.target.value
+                                                )
+                                            }
+                                        />
                                     </div>
                                 </div>
-                            )}
-                    </div>
+                            </div>
+                        )}
+                </div>
 
-                    <div className="button-section">
-                        <ButtonCustomizer
-                            text={buttonText}
-                            proSetting={proSetting}
-                            setting={setting?.customize_btn}
-                            onChange={(
-                                key,
-                                value,
-                                isRestoreDefaults = false
-                            ) => {
-                                const previousSetting =
-                                    setting?.customize_btn || {};
-                                if (isRestoreDefaults) {
-                                    onChange('customize_btn', value);
-                                } else {
-                                    onChange('customize_btn', {
-                                        ...previousSetting,
-                                        [key]: value,
-                                    });
-                                }
-                            }}
-                        />
-                    </div>
+                <div className="button-section">
+                    <ButtonCustomizer
+                        text={buttonText}
+                        proSetting={proSetting}
+                        setting={setting?.customize_btn}
+                        onChange={(
+                            key,
+                            value,
+                            isRestoreDefaults = false
+                        ) => {
+                            const previousSetting =
+                                setting?.customize_btn || {};
+                            if (isRestoreDefaults) {
+                                onChange('customize_btn', value);
+                            } else {
+                                onChange('customize_btn', {
+                                    ...previousSetting,
+                                    [key]: value,
+                                });
+                            }
+                        }}
+                    />
                 </div>
             </div>
         </div>
