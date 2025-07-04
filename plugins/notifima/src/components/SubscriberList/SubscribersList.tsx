2b79ba7d76d4a3a7656e792b93dc5b3eba66f6cc
Buddhadeb Roy <roybuddhadeb2003@gmail.com>
2025-07-04 15:33:36 +0530

    css

--- plugins/notifima/src/components/SubscriberList/SubscribersList.tsx
+++ plugins/notifima/src/components/SubscriberList/SubscribersList.tsx
@@ -8,47 +8,44 @@ const SubscribersList: React.FC = () => {
     const [ openDialog, setOpenDialog ] = useState( false );
     return (
         <>
-            <div id="subscriber-list-table">
-                <div className="free-reports-download-section">
-                    <h2 className="section-heading">
-                        { __(
-                            'Download product wise subscriber data.',
-                            'notifima'
-                        ) }
-                    </h2>
-                    <button>
-                        <a href={ appLocalizer.export_button }>
-                            { __( 'Download CSV', 'notifima' ) }
-                        </a>
-                    </button>
-                    <p
-                        className="description"
-                        dangerouslySetInnerHTML={ {
-                            __html: "This CSV file contains all subscriber data from your site. Upgrade to <a href='https://notifima.com/pricing/?utm_source=wpadmin&utm_medium=pluginsettings&utm_campaign=notifima' target='_blank'>Notifima Pro</a> to generate CSV files based on specific products or users.",
-                        } }
-                    ></p>
-                </div>
-                <Dialog
-                    className="admin-module-popup"
-                    open={ openDialog }
-                    onClose={ () => {
-                        setOpenDialog( false );
+            <div className="download-csv-wrapper">
+                <h2 className="section-heading">
+                    { __(
+                        'Download product wise subscriber data.',
+                        'notifima'
+                    ) }
+                </h2>
+                <a className='admin-btn btn-purple' href={ appLocalizer.export_button }>
+                    { __( 'Download CSV', 'notifima' ) }
+                </a>
+                
+                <p
+                    className="description"
+                    dangerouslySetInnerHTML={ {
+                        __html: "This CSV file contains all subscriber data from your site. Upgrade to <a href='https://notifima.com/pricing/?utm_source=wpadmin&utm_medium=pluginsettings&utm_campaign=notifima' target='_blank'>Notifima Pro</a> to generate CSV files based on specific products or users.",
                     } }
-                    aria-labelledby="form-dialog-title"
-                >
-                    <span
-                        className="admin-font adminlib-cross"
-                        onClick={ () => setOpenDialog( false ) }
-                    ></span>
-                    <Popup />
-                </Dialog>
-                <div
-                    className="subscriber-img"
-                    onClick={ () => {
-                        setOpenDialog( true );
-                    } }
-                ></div>
+                ></p>
             </div>
+            <Dialog
+                className="admin-module-popup"
+                open={ openDialog }
+                onClose={ () => {
+                    setOpenDialog( false );
+                } }
+                aria-labelledby="form-dialog-title"
+            >
+                <span
+                    className="admin-font adminlib-cross"
+                    onClick={ () => setOpenDialog( false ) }
+                ></span>
+                <Popup />
+            </Dialog>
+            <div
+                className="subscriber-img"
+                onClick={ () => {
+                    setOpenDialog( true );
+                } }
+            ></div>
         </>
     );
 };
