4f1dedf0b13718a82f56b0aa19e6ddd8addc2b3e
user-promi <promita@dualcube.com>
2025-07-16 14:10:06 +0530

    update image

--- plugins/moowoodle/src/components/Settings/Settings.tsx
+++ plugins/moowoodle/src/components/Settings/Settings.tsx
@@ -1,6 +1,6 @@
 /* global appLocalizer */
-import Brand from '../../assets/images/Brand.png';
-import BrandSmall from '../../assets/images/Brand-small.png';
+import Brand from '../../assets/images/brand.png';
+import BrandSmall from '../../assets/images/brand-small.png';
 import React, { useEffect, JSX } from 'react';
 import { __ } from '@wordpress/i18n';
 // Context
