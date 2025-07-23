a41b513df5a7aea560ded00dcc60ca904d0a1626
user-promi <promita@dualcube.com>
2025-07-23 10:24:56 +0530

    fixed css and change log

--- plugins/notifima/config.php
+++ plugins/notifima/config.php
@@ -9,6 +9,6 @@ defined( 'ABSPATH' ) || exit;
 
 define( 'NOTIFIMA_PRO_SHOP_URL', 'https://notifima.com/pricing/?utm_source=wpadmin&utm_medium=pluginsettings&utm_campaign=notifima' );
 
-define( 'NOTIFIMA_PLUGIN_VERSION', '3.0.3' );
+define( 'NOTIFIMA_PLUGIN_VERSION', '3.0.4' );
 
 define( 'PLUGIN_SLUG', 'notifima' );
