927ba23ad2d664a24592cbf784b74f277229804c
Ankan Layek <ankanlayek2004@gmail.com>
2025-08-02 12:37:39 +0530

    fix

--- plugins/catalogx/src/components/QuoteRequests/quoteRequests.tsx
+++ plugins/catalogx/src/components/QuoteRequests/quoteRequests.tsx
@@ -21,7 +21,7 @@ export default function QuotesList() {
                     aria-labelledby="form-dialog-title"
                 >
                     <span
-                        className="admin-font adminlib-cross stock-manager-popup-cross"
+                        className="admin-font adminlib-cross"
                         onClick={ () => {
                             setOpenDialog( false );
                         } }
