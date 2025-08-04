86df9b89d6480905efef82f71188f9cf2fee817c
user-promi <promita@dualcube.com>
2025-08-04 11:24:46 +0530

    add mowoodle wpml and scss and change log

--- plugins/moowoodle/config.php
+++ plugins/moowoodle/config.php
@@ -5,6 +5,6 @@
  * @package MooWoodle
  */
 
-define( 'MOOWOODLE_PLUGIN_VERSION', '3.3.3' );
+define( 'MOOWOODLE_PLUGIN_VERSION', '3.3.4' );
 define( 'MOOWOODLE_PRO_SHOP_URL', 'https://dualcube.com/product/moowoodle-pro/?utm_source=wpadmin&utm_medium=pluginsettings&utm_campaign=moowoodle' );
 define( 'PLUGIN_SLUG', 'moowoodle' );
