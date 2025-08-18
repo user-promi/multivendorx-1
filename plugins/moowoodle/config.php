261b7a450649ae5db51853f5987a79e59d9d20d0
user-promi <promita@dualcube.com>
2025-08-18 17:34:51 +0530

    update

--- plugins/moowoodle/config.php
+++ plugins/moowoodle/config.php
@@ -7,4 +7,4 @@
 
 define( 'MOOWOODLE_PLUGIN_VERSION', '3.3.4' );
 define( 'MOOWOODLE_PRO_SHOP_URL', 'https://dualcube.com/product/moowoodle-pro/?utm_source=wpadmin&utm_medium=pluginsettings&utm_campaign=moowoodle' );
-define( 'PLUGIN_SLUG', 'moowoodle' );
+define( 'MOOWOODLE_PLUGIN_SLUG', 'moowoodle' );
