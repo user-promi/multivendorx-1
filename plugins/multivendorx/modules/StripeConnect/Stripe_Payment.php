d6ccb652744e9e925006ae5c5065d17c47f31b62
user-promi <promita@dualcube.com>
2025-09-03 10:31:29 +0530

    update

---
+++ plugins/multivendorx/modules/StripeConnect/Stripe_Payment.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace MultiVendorX\StripeConnect;
+
+
+defined('ABSPATH') || exit;
+
+class Stripe_Payment {
+
+    public function get_id() {
+        return 'stripe-connect';
+    }
+
+    public function get_settings() {
+        return [
+            'icon'      => 'ST',
+            'id'        => $this->get_id(),
+            'label'     => 'Stripe Connect',
+            'connected' => true,
+            'desc'      => 'Full marketplace solution with instant payouts, comprehensive dispute handling, and global coverage. Best for established marketplaces.',
+            'formFields' => [
+                [
+                    'key'   => 'payment_mode',
+                    'type'  => 'setting-toggle',
+                    'label' => __('Payment Mode', 'multivendorx'),
+                    'options' => [
+                        ['key' => 'test', 'label' => __('Test', 'multivendorx'), 'value' => 'test'],
+                        ['key' => 'live', 'label' => __('Live', 'multivendorx'), 'value' => 'live'],
+                    ]
+                ],
+                [
+                    'key'         => 'api_key',
+                    'type'        => 'text',
+                    'label'       => 'API Key',
+                    'placeholder' => 'Enter API Key',
+                ],
+                [
+                    'key'         => 'secret_key',
+                    'type'        => 'password',
+                    'label'       => 'Secret Key',
+                    'placeholder' => 'Enter Secret Key',
+                ]
+            ]
+        ];
+    }
+
+    public function get_store_payment_settings() {
+        return [
+            'id'    => $this->get_id(),
+            'label' => __('Stripe Connect', 'multivendorx'),
+            'fields' => []
+        ];
+    }
+
+    public function process_payment() {
+        
+    }
+}
