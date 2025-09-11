cca3bda9618cd4b0c9d8970b5bd899c45a67e2d5
moumitahalder <moumita.h@dualcube.com>
2025-09-11 15:59:30 +0530

    Update coupon.ts

--- plugins/multivendorx/src/components/Settings/Finance/coupon.ts
+++ plugins/multivendorx/src/components/Settings/Finance/coupon.ts
@@ -12,7 +12,7 @@ export default {
             key: 'commission_include_coupon',
             label: __( 'Who will bear the coupon cost', 'multivendorx' ),
             type: 'setting-toggle',
-            desc: __('<ul><li>Stores - commission is calculated after the coupon is applied.Example: Product $100 – 20% coupon = $80 → Store earns 80% of $80 = $64</li><li>Admin - Commission is calculated on the original price, admin covers the discount.Example: Product $100 – 20% coupon = $80 → Store earns 80% of $100 = $80</li></ul>','multivendorx'),
+            desc: __('<ul><li>Stores - commission is calculated after the coupon is applied.Example: Product $100 – 20% coupon = $80 → Store earns 80% of $80 = $64</li><li>Admin - Commission is calculated on the original price, admin covers the discount.Example: Product $100 – 20% coupon = $80 → Store earns 80% of $100 = $80</li><li>His His Whose Whose – Discount is split according to commission rates. Both admin and store bear their share.Example: Product $100 – 20% coupon = $80 Admin commission 10% → Admin bears 10% of $20 = $2 Store bears remaining 90% → Store bears $18 of the discount</li></ul>','multivendorx'),
             options: [
                 {
                     key: 'store_coupon',
@@ -22,6 +22,11 @@ export default {
 				{
                     key: 'admin_coupon',
                     label: __('Admin', 'multivendorx'),
+                    value: 'mannual',
+                },
+				{
+                    key: 'his_his-whose_whose',
+                    label: __('His His Whose Whose', 'multivendorx'),
                     value: 'mannual',
                 },
             ],
