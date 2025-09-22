f262bda787e43cc111c4f5e3c362b80418a5eaaa
buddhadebroy <roybuddhadeb2003@gmail.com>
2025-09-22 17:45:36 +0530

    add task

---
+++ plugins/multivendorx/src/dashboard/general.tsx
@@ -0,0 +1,25 @@
+const History: React.FC = () => {
+    return (
+        <>
+            {/* page title start */}
+            < div className="page-title-wrapper" >
+                <div className="page-title">
+                    <div className="title">
+                        General
+                    </div>
+                </div>
+            </div > {/* page title end */}
+
+            <div className="container-wrapper">
+                <div className="card-wrapper">
+                    <div className="card-content">
+                        <div className="card-title">General</div>
+                        
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+};
+
+export default History;
