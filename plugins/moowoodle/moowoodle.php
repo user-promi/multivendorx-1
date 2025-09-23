4de5ff40ee97823c236d2c5b223919a8028822df
user-promi <promita@dualcube.com>
2025-09-23 16:05:23 +0530

    change log add

--- plugins/moowoodle/moowoodle.php
+++ plugins/moowoodle/moowoodle.php
@@ -4,12 +4,12 @@
  * Plugin URI: https://dualcube.com/?utm_source=wpadmin&utm_medium=pluginsettings&utm_campaign=moowoodle
  * Description: The MooWoodle plugin is an extention of WooCommerce that acts as a bridge between WordPress/Woocommerce and Moodle.
  * Author: DualCube
- * Version: 3.3.4
+ * Version: 3.3.5
  * Author URI: https://dualcube.com/?utm_source=wpadmin&utm_medium=pluginsettings&utm_campaign=moowoodle
  * Requires at least: 6.0.0
  * Tested up to: 6.8.2
  * WC requires at least: 8.4.0
- * WC tested up to: 10.0.4
+ * WC tested up to: 10.2.1
  *
  * Text Domain: moowoodle
  * Domain Path: /languages/
