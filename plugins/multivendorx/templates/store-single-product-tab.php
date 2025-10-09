c87cc259064e6f7af0266f83d53b8302125217d5
buddhadebroy <roybuddhadeb2003@gmail.com>
2025-10-09 13:10:35 +0530

    store info

--- plugins/multivendorx/templates/store-single-product-tab.php
+++ plugins/multivendorx/templates/store-single-product-tab.php
@@ -6,7 +6,6 @@ $html = '';
 
 $store = StoreUtil::get_products_vendor($product->get_id());
 if ($store) {
-    $html .= '<div class="single-product-vendor">';
     $html .= apply_filters('mvx_before_seller_info_tab', '');
 
     $details = MultiVendorX()->frontend->show_store_info($product->get_id());
@@ -25,40 +24,39 @@ if ($store) {
         }
 
         // Start vendor info block
-        $html .= '<div class="mvx-vendor-info">';
-        $html .= $details['logo_html'];
-        $html .= '<h2 class="vendor-heading">' . $heading_text . '</h2>';
+        $html .= '<div class="product-store-info">';
+        $html .= '<div class="header">' . $details['logo_html'];
+        $html .= '<div class="heading-wrapper"> <h4 class="heading">' . $heading_text . '</h4>';
 
-        $rating_html = '<div class="vendor-rating">⭐️⭐️⭐️⭐️☆ (4.0 / 5)</div>';
+        $rating_html = '<div class="admin-badge yellow"> <i class="adminlib-star"></i> 4.0 <span>(4523)</span></div> ' 
+                        // . apply_filters('mvx_follow_button_html', '', $store->get_id(), get_current_user_id()) 
+                        . '</div> </div>';
         $html .= $rating_html;
 
         if (!empty($details['address'])) {
-            $html .= '<p class="vendor-address"><strong>' . __('Address:', 'multivendorx') . '</strong> ' . esc_html($details['address']) . '</p>';
+            $html .= '<div class="store-info"> <i class="adminlib-location"></i>' . esc_html($details['address']) . '</div>';
         }
 
         $store_details = MultiVendorX()->setting->get_setting('store_branding_details', []);
         if (in_array('show_store_description', $store_details) && !empty($details['description'])) {
-            $html .= '<div class="vendor-description">' . apply_filters('the_content', $details['description']) . '</div>';
+            $html .= '<div class="store-description">' . apply_filters('the_content', $details['description']) . '</div>';
         }
 
         if (!empty($details['phone'])) {
-            $html .= '<p class="vendor-phone"><strong>' . __('Phone:', 'multivendorx') . '</strong> ' . esc_html($details['phone']) . '</p>';
+            $html .= '<div class="store-info"><i class="adminlib-form-phone"></i>' . esc_html($details['phone']) . '</div>';
         }
 
         if (!empty($details['email'])) {
-            $html .= '<p class="vendor-email"><strong>' . __('Email:', 'multivendorx') . '</strong> ' . esc_html($details['email']) . '</p>';
+            $html .= '<div class="store-info"><i class="adminlib-mail"></i> ' . esc_html($details['email']) . '</div>';
         }
 
-        $html .= apply_filters('mvx_follow_button_html', '', $store->get_id(), get_current_user_id());
-
-        $html .= '<p><a href="' . esc_url(MultiVendorX()->store->storeutil->get_store_url($store->get_id())) . '">'
-            . sprintf(__('More Products from %1$s', 'multivendorx'), $store->get('name')) . '</a></p>';
+        $html .= '<a class="button product_type_simple" href="' . esc_url(MultiVendorX()->store->storeutil->get_store_url($store->get_id())) . '">'
+            . sprintf(__('More Products from %1$s', 'multivendorx'), $store->get('name')) . '</a>';
 
         $html .= '</div>'; // .mvx-vendor-info
     }
 
     $html .= apply_filters('mvx_after_seller_info_tab', '');
-    $html .= '</div>'; // .single-product-vendor
 
     echo $html;
 
