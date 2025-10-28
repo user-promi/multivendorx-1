d4c31b6d7371656361e8626441f2dca1598ec80f
buddhadebroy <roybuddhadeb2003@gmail.com>
2025-10-28 21:48:26 +0530

    css fixed

--- plugins/multivendorx/classes/Payments/BankTransfer.php
+++ plugins/multivendorx/classes/Payments/BankTransfer.php
@@ -18,7 +18,7 @@ class BankTransfer {
 
     public function get_settings() {
         return [
-            'icon'      => 'adminlib-payment',
+            'icon'      => 'adminlib-bank',
             'id'        => $this->get_id(),
             'label'     => 'Bank Transfer',
             'enableOption' => true,
