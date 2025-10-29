cbabfe960b10ebe0be748c9cc6ef0717b74e6d48
moumitahalder <moumita.h@dualcube.com>
2025-10-29 19:58:29 +0530

    Few changes

--- plugins/multivendorx/src/components/Settings/Finance/Commissions.ts
+++ plugins/multivendorx/src/components/Settings/Finance/Commissions.ts
@@ -292,6 +292,7 @@ export default {
                 'Define a fee to cover platform costs. Apply a fixed, percentage, or combined rate. Choose whether it’s paid by the customer at checkout or deducted from the store’s commission.',
                 'multivendorx'
             ),
+            desc: __('<ul><li><strong>Customer</strong> – The marketplace fee is added to the customer’s order total at checkout.</li><li><strong>Store</strong> – The marketplace fee is deducted from the store’s commission during payout.</li></ul>','multivendorx'),
             nestedFields: [
                 {
                     key: 'commission_fixed',
