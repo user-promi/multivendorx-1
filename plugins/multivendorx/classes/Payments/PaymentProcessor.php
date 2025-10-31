a49a110d803c83a41cafe202ba36da276e1f3909
user-promi <promita@dualcube.com>
2025-10-31 21:48:39 +0530

    withdrawal request

--- plugins/multivendorx/classes/Payments/PaymentProcessor.php
+++ plugins/multivendorx/classes/Payments/PaymentProcessor.php
@@ -81,7 +81,7 @@ class PaymentProcessor {
                 $format
             );
 
-            return false;
+            return;
         }
             
         do_action("multivendorx_process_{$payment_method}_payment", $store_id, $amount, $order_id, $transaction_id, $note);
