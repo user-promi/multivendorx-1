fe9782967e14cc7d5f012c52d390aa70e8eb997c
moumitahalder <moumita.h@dualcube.com>
2025-11-12 11:47:59 +0530

    texts

--- plugins/multivendorx/src/components/Settings/Marketplace/notificationConfiguration.ts
+++ plugins/multivendorx/src/components/Settings/Marketplace/notificationConfiguration.ts
@@ -9,32 +9,36 @@ export default {
     icon: 'adminlib-user-network-icon',
     submitUrl: 'settings',
     modal: [
+        // ===== Email Section =====
         {
-            key: 'name',
-            type: 'text',
-            label: __( 'Name', 'multivendorx' ),
-            desc: __(
-                '',
-                'multivendorx'
-            ),
-        },  
+            key: 'email_section',
+            type: 'section',
+            hint: __('The email provider used to deliver marketplace notifications is specified here.', 'multivendorx'),
+        },
         {
-            key: 'email',
+            key: 'sender_name',
             type: 'text',
-            label: __( 'Sender Email', 'multivendorx' ),
-            desc: __(
-                '',
-                'multivendorx'
-            ),
-        },  
+            label: __('Sender Name', 'multivendorx'),
+            desc: __('The name displayed in outgoing emails is assigned. Test emails can be sent to confirm delivery.', 'multivendorx'),
+        },
         {
-            key: 'phone_number',
+            key: 'sender_email',
             type: 'text',
-            label: __( 'Sender Phone', 'multivendorx' ),
-            desc: __(
-                '',
-                'multivendorx'
-            ),
-        },  
+            label: __('Sender Email', 'multivendorx'),
+            desc: __('The email address from which notifications are sent is provided. Test emails can be sent to confirm delivery.', 'multivendorx'),
+        },
+
+        // ===== SMS Section =====
+        {
+            key: 'sms_section',
+            type: 'section',
+            hint: __('The SMS gateway used to send text notifications is defined here.', 'multivendorx'),
+        },
+        {
+            key: 'sender_phone',
+            type: 'text',
+            label: __('Sender Phone / ID', 'multivendorx'),
+            desc: __('The phone number or sender ID used for outgoing SMS is assigned. Test SMS messages can be sent to confirm delivery.', 'multivendorx'),
+        },
     ]
 }
