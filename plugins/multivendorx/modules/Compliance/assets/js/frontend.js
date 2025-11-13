46cd9d96df7254ae37564d0ed8fc28f8a75e9b05
Amit Sarkar <88242426+imroxygen@users.noreply.github.com>
2025-11-13 10:37:24 +0530

    abuse report

--- plugins/multivendorx/modules/Compliance/assets/js/frontend.js
+++ plugins/multivendorx/modules/Compliance/assets/js/frontend.js
@@ -42,15 +42,23 @@ jQuery(function($){
         if ($btn.prop('disabled')) return;
 
         var $form = $btn.closest('.report-abuse-form');
-        var name  = $form.find('.report_abuse_name').val();
-        var email = $form.find('.report_abuse_email').val();
+        var name  = $form.find('.report_abuse_name').val().trim();
+        var email = $form.find('.report_abuse_email').val().trim();
         var reason = $form.find('.report_abuse_reasons_wrapper input[type=radio]:checked').val();
-        var msg   = (reason === 'Other') ? $form.find('.report_abuse_msg').val() : reason;
+        var msg   = (reason === 'Other') ? $form.find('.report_abuse_msg').val().trim() : reason;
         var pid   = $form.find('.report_abuse_product_id').val();
         var $msgBox = $form.find('.report-abuse-msg-box');
 
+        // Simple email regex validation
+        var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
         if(!name || !email || !reason || !msg){
             $msgBox.html('<span style="color:red;">All fields are required.</span>');
+            return;
+        }
+
+        if(!emailRegex.test(email)){
+            $msgBox.html('<span style="color:red;">Please enter a valid email address.</span>');
             return;
         }
 
@@ -71,17 +79,32 @@ jQuery(function($){
             },
             success: function(res){
                 if(res.success){
-                    $btn.replaceWith('');
-                    $msgBox.html('<span>'+res.data+'</span>');
+                    $msgBox.html('<span style="color:green;">'+res.data+'</span>');
+
+                    // Hide the form after success
+                    setTimeout(function(){
+                        $form.slideUp();
+                    }, 1000);
+
+                    // Reset form fields
+                    $form.find('input[type=text], input[type=email], textarea').val('');
+                    $form.find('input[type=radio]').prop('checked', false);
+                    $form.find('.report-abuse-custom-msg').hide();
+
+                    // Reset button
+                    $btn.prop('disabled', false);
+                    $btn.find('.btn-text').show();
+                    $btn.find('.btn-spinner').hide();
+
                 } else {
-                    $msgBox.html('<span>'+res.data+'</span>');
+                    $msgBox.html('<span style="color:red;">'+res.data+'</span>');
                     $btn.prop('disabled', false);
                     $btn.find('.btn-text').show();
                     $btn.find('.btn-spinner').hide();
                 }
             },
             error: function(){
-                $msgBox.html('<span>Something went wrong. Try again.</span>');
+                $msgBox.html('<span style="color:red;">Something went wrong. Try again.</span>');
                 $btn.prop('disabled', false);
                 $btn.find('.btn-text').show();
                 $btn.find('.btn-spinner').hide();
