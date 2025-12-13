ff34325275b4d858c406a3a1b6d3b96e396e617a
Amit Sarkar <88242426+imroxygen@users.noreply.github.com>
2025-12-13 15:01:00 +0530

    conflict resolve

--- plugins/multivendorx/src/components/Membership/membership.tsx
+++ plugins/multivendorx/src/components/Membership/membership.tsx
@@ -3,83 +3,62 @@ import { Tabs } from 'zyra';
 
 import MessageAndMail from './messageAndMail';
 
+
 const Memberships = () => {
-	const location = useLocation();
-	const hash = location.hash.replace(/^#/, '');
+    const location = useLocation();
+    const hash = location.hash.replace(/^#/, '');
 
-	const hashParams = new URLSearchParams(hash);
-	const currentTab = hashParams.get('subtab') || 'payment-membership-message';
+    const hashParams = new URLSearchParams(hash);
+    const currentTab = hashParams.get('subtab') || 'payment-membership-message';
 
-	const prepareUrl = (tabId: string) =>
-		`?page=multivendorx#&tab=memberships&subtab=${tabId}`;
+    const prepareUrl = (tabId: string) => `?page=multivendorx#&tab=memberships&subtab=${tabId}`;
 
-	const tabData = [
-		{
-			type: 'file',
-			content: {
-				id: 'payment-membership-message',
-				name: 'Basic Plan Details',
-				desc: 'Site errors and events are logged for easy troubleshooting.',
-				icon: 'adminlib-credit-card',
-			},
-		},
-		{
-			type: 'file',
-			content: {
-				id: 'payment-membership-design',
-				name: 'MultivendorX Pro Features',
-				desc: 'PayPal Payout makes it easy for you to pay multiple sellers at the sametime.',
-				icon: 'adminlib-credit-card',
-			},
-		},
-		// {
-		//     type: 'file',
-		//     content: {
-		//         id: 'payment-membership-subscribers',
-		//         name: 'Subscribers',
-		//         desc: 'Connect to vendors stripe account and make hassle-free transfers as scheduled.',
-		//         icon: 'adminlib-credit-card',
-		//     },
-		// },
-		// {
-		//     type: 'file',
-		//     content: {
-		//         id: 'payment-membership-plans',
-		//         name: 'Plans',
-		//         desc: 'Connect to vendors stripe account and make hassle-free transfers as scheduled',
-		//         icon: 'adminlib-credit-card',
-		//     },
-		// },
-	];
+    const tabData = [
+        {
+            type: 'file',
+            content: {
+                id: 'payment-membership-message',
+                name: 'Basic Plan Details',
+                desc: 'Site errors and events are logged for easy troubleshooting.',
+                icon: 'adminlib-credit-card',
 
-	const getForm = (tabId: string) => {
-		switch (tabId) {
-			case 'payment-membership-message':
-				return <MessageAndMail id="" />;
-			case 'payment-membership-design':
-				return <h1>design</h1>;
-			// case 'payment-membership-subscribers':
-			//     return <h1>Subscriber</h1>;
-			// case 'payment-membership-plans':
-			//     return <h1>Plans</h1>;
-			default:
-				return <div></div>;
-		}
-	};
+            },
+        },
+        {
+            type: 'file',
+            content: {
+                id: 'payment-membership-design',
+                name: 'MultivendorX Pro Features',
+                desc: 'PayPal Payout makes it easy for you to pay multiple sellers at the sametime.',
+                icon: 'adminlib-credit-card',
+            },
+        },
+    ];
 
-	return (
-		<>
-			<Tabs
-				tabData={tabData}
-				currentTab={currentTab}
-				getForm={getForm}
-				prepareUrl={prepareUrl}
-				appLocalizer={appLocalizer}
-				Link={Link}
-				settingName={'Memberships'}
-			/>
-		</>
-	);
+    const getForm = (tabId: string) => {
+        switch (tabId) {
+            case 'payment-membership-message':
+                return <MessageAndMail id=''/>;
+            case 'payment-membership-design':
+                return <h1>design</h1>;
+            default:
+                return <div></div>;
+        }
+    };
+
+    return (
+        <>
+            <Tabs
+                tabData={tabData}
+                currentTab={currentTab}
+                getForm={getForm}
+                prepareUrl={prepareUrl}
+                appLocalizer={appLocalizer}
+                Link={Link}
+                settingName={'Memberships'}
+            />
+        </>
+    );
 };
 
 export default Memberships;
