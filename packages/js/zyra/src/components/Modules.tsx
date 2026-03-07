02d1bbd5f5f6ec56fb23429c44814de7e41a87d0
buddhadebroy <roybuddhadeb2003@gmail.com>
2026-03-07 21:31:01 +0530

    code remove

--- packages/js/zyra/src/components/Modules.tsx
+++ packages/js/zyra/src/components/Modules.tsx
@@ -97,11 +97,8 @@ const Modules: React.FC<ModuleProps> = ({
             .map(item => ({ id: item.id, label: item.label })),
     ];
 
-    const isModuleAvailable = (module: Module) => {
-        if (!module.proModule) return true;
-        
-        return appLocalizer.khali_dabba === true;
-    };
+    const isModuleAvailable = (module: Module) =>
+        module.proModule ? appLocalizer.khali_dabba ?? false : true;
 
     const handleOnChange = (
         event: string[],
@@ -116,12 +113,9 @@ const Modules: React.FC<ModuleProps> = ({
             return;
         }
 
-        if (module.reqPluging?.some(plugin => {
-            if (!Array.isArray(appLocalizer.active_plugins)) {
-                return true;
-            }
-            return !appLocalizer.active_plugins.includes(plugin.slug);
-        })) {
+        if (module.reqPluging?.some(
+            plugin => !appLocalizer.active_plugins?.includes(plugin.slug)
+        )) {
             openRequirePopup(module);
             return;
         }
