2195310c245542a7576dcef45d572e927711aeab
buddhadebroy <roybuddhadeb2003@gmail.com>
2026-03-14 16:46:46 +0530

    elementor

--- plugins/multivendorx/modules/Elementor/Admin.php
+++ plugins/multivendorx/modules/Elementor/Admin.php
@@ -112,37 +112,146 @@ class Admin {
     private function get_default_store_elementor_data() {
 
         $data = array(
-            array(
-                'id'       => 'store-header',
-                'elType'   => 'container',
-                'settings' => array(
-                    'content_width'  => 'boxed',
-                    'flex_direction' => 'column',
-                    'gap'            => '10',
+        // store banner start
+         array(
+            'id'       => 'main-store-container-' . uniqid(),
+            'elType'   => 'container',
+            'settings' => array(
+                'content_width'  => 'boxed',
+                'flex_direction' => 'row',
+                'gap'            => '10',
+            ),
+            'elements' => array(
+                 array(
+                    'id'         => 'store-logo-' . uniqid(),
+                    'elType'     => 'widget',
+                    'widgetType' => 'multivendorx_store_banner',
+                    'elements'   => array(),
                 ),
-                'elements' => array(
-                    array(
-                        'id'         => 'store-name',
-                        'elType'     => 'widget',
-                        'widgetType' => 'multivendorx_store_name',
-                        'settings'   => array(
-                            'html_tag' => 'h1',
-                        ),
-                        'elements'   => array(),
-                    ),
-                    array(
-                        'id'         => 'store-description',
-                        'elType'     => 'widget',
-                        'widgetType' => 'multivendorx_store_description',
-                        'settings'   => array(
-                            'empty_text' => 'This store has not added a description yet.',
-                        ),
-                        'elements'   => array(),
-                    ),
+            )
+        ), // store banner start
 
+        // Main container
+        array(
+            'id'       => 'main-store-container-' . uniqid(),
+            'elType'   => 'container',
+            'settings' => array(
+                'content_width'  => 'boxed',
+                'flex_direction' => 'row',
+                'gap'            => '10',
+            ),
+            'elements' => array(
+                // store details container 
+                array(
+                    'id'       => 'secondary-container-' . uniqid(),
+                    'elType'   => 'container',
+                    'settings' => array(
+                        'content_width'  => 'boxed',
+                        'flex_direction' => 'row',  // This makes it a row container
+                        'gap'            => '10',
+                    ),
+                    'elements' => array(
+                        // Left container in the row
+                        // Store Logo or Image widget in right container
+                                array(
+                                    'id'         => 'store-logo-' . uniqid(),
+                                    'elType'     => 'widget',
+                                    'widgetType' => 'multivendorx_store_logo',
+                                    'settings'   => array(
+                                        'image_size' => 'thumbnail',
+                                        'align'      => 'center',
+                                    ),
+                                    'elements'   => array(),
+                                ),
+                        // Right container in the row
+                        array(
+                            'id'       => 'right-container-' . uniqid(),
+                            'elType'   => 'container',
+                            'settings' => array(
+                                'content_width'  => 'boxed',
+                                'flex_direction' => 'column',
+                                'width'          => array(
+                                    'size' => 50,
+                                    'unit' => '%'
+                                ),
+                                'gap'            => '10',
+                            ),
+                            'elements' => array(
+                                // Store Name widget
+                                array(
+                                    'id'         => 'store-name-' . uniqid(),
+                                    'elType'     => 'widget',
+                                    'widgetType' => 'multivendorx_store_name',
+                                    'settings'   => array(
+                                        'html_tag' => 'h1',
+                                    ),
+                                    'elements'   => array(),
+                                ),
+                                // Store info
+                                array(
+                                    'id'         => 'store-info-widget-' . uniqid(),
+                                    'elType'     => 'widget',
+                                    'widgetType' => 'multivendorx_store_info',
+                                    'elements'   => array(),
+                                ),
+                                // Store Description
+                                array(
+                                    'id'         => 'store-description-' . uniqid(),
+                                    'elType'     => 'widget',
+                                    'widgetType' => 'multivendorx_store_description',
+                                    'settings'   => array(
+                                        'empty_text' => 'This store has not added a description yet.',
+                                    ),
+                                    'elements'   => array(),
+                                ),                         
+                            ),
+                        ),
+                    ),
+                ),
+                // right buttons container (right)
+                array(
+                    'id'       => 'secondary-container-' . uniqid(),
+                    'elType'   => 'container',
+                    'settings' => array(
+                        'content_width'  => 'boxed',
+                        'justify_content' => 'flex-end', 
+                        'align_items' => 'center',
+                        'gap'            => '10',
+                    ),
+                    'elements' => array(
+                        // Store Follow Button Widget
+                        array(
+                            'id'         => 'store-follow-button-' . uniqid(),
+                            'elType'     => 'widget',
+                            'widgetType' => 'multivendorx_store_follow',
+                            'settings'   => array(
+                                'text' => 'Follow',
+                            ),
+                            'elements'   => array(),
+                        ),
+                    ),
                 ),
             ),
-        );
+        ),  // Main container end
+
+        // store tab start
+        array(
+            'id'       => 'main-store-container-' . uniqid(),
+            'elType'   => 'container',
+            'settings' => array(
+                'content_width'  => 'boxed',
+                'gap'            => '10',
+            ),
+            'elements' => array(
+                 array(
+                    'id'         => 'multivendorx-store-tab-' . uniqid(),
+                    'elType'     => 'widget',
+                    'widgetType' => 'multivendorx_Store_Tab',
+                    'elements'   => array(),
+                ),
+            )
+        ), // store tab end
+    );
 
         return wp_json_encode( $data );
     }
