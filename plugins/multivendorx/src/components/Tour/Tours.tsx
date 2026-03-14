a8535ccb3b8ab69c44eb0bee8d178cadf1c7b3c5
divineshiva003 <raishivam.in2k3@gmail.com>
2026-03-14 16:17:54 +0530

    Tours updated

--- plugins/multivendorx/src/components/Tour/Tours.tsx
+++ plugins/multivendorx/src/components/Tour/Tours.tsx
@@ -4,82 +4,68 @@ export const getTourSteps = (appLocalizer: any) => [
 	{
 		selector: '.card-content',
 		placement: 'auto',
-		content: () => ({
-			title: __('Dashboard', 'multivendorx'),
-			description: __('View and configure your disbursement settings here.', 'multivendorx'),
-			nextBtn: {
-				link: `${appLocalizer.site_url}/wp-admin/admin.php?page=multivendorx#&tab=settings&subtab=commissions`,
-				step: 1
-			}
-		}),
+		title: __('Dashboard', 'multivendorx'),
+		description: __('View and configure your disbursement settings here.', 'multivendorx'),
+		next: {
+			link: `${appLocalizer.site_url}/wp-admin/admin.php?page=multivendorx#&tab=settings&subtab=commissions`,
+			step: 1
+		}
 	},
 	{
 		selector: '.form-group:has(.settings-form-label[for="commission_type"])',
 		placement: 'right',
-		content: () => ({
-			title: __('Marketplace commissions', 'multivendorx'),
-			description: __('View and configure your disbursement settings here.', 'multivendorx'),
-			nextBtn: {
-				link: `${appLocalizer.site_url}/wp-admin/admin.php?page=multivendorx#&tab=settings&subtab=payouts`,
-				step: 2
-			}
-		}),
+		title: __('Marketplace commissions', 'multivendorx'),
+		description: __('View and configure your disbursement settings here.', 'multivendorx'),
+		next: {
+			link: `${appLocalizer.site_url}/wp-admin/admin.php?page=multivendorx#&tab=settings&subtab=payouts`,
+			step: 2
+		}
 	},
 	{
 		selector: '.form-group:has(.settings-form-label[for="disbursement_order_status"])',
 		placement: 'right',
-		content: () => ({
-			title: __('Store Configure', 'multivendorx'),
-			description: __('View and configure your disbursement settings here.', 'multivendorx'),
-			nextBtn: {
-				link: `${appLocalizer.site_url}/wp-admin/admin.php?page=multivendorx#&tab=settings&subtab=onboarding`,
-				step: 3
-			}
-		}),
+		title: __('Store Configure', 'multivendorx'),
+		description: __('View and configure your disbursement settings here.', 'multivendorx'),
+		next: {
+			link: `${appLocalizer.site_url}/wp-admin/admin.php?page=multivendorx#&tab=settings&subtab=onboarding`,
+			step: 3
+		}
 	},
 	{
 		selector: '.form-group:has(.settings-form-label[for="approve_store"])',
 		placement: 'right',
-		content: () => ({
-			title: __('Commission Value', 'multivendorx'),
-			description: __('View and configure your disbursement settings here.', 'multivendorx'),
-			nextBtn: {
-				link: `${appLocalizer.site_url}/wp-admin/admin.php?page=multivendorx#&tab=modules`,
-				step: 4
-			}
-		}),
+		title: __('Commission Value', 'multivendorx'),
+		description: __('View and configure your disbursement settings here.', 'multivendorx'),
+		next: {
+			link: `${appLocalizer.site_url}/wp-admin/admin.php?page=multivendorx#&tab=modules`,
+			step: 4
+		}
 	},
 	{
-		selector: '.category-filter .category-item:nth-child(3) ',
+		selector: '.category-filter .category-item:nth-child(3)',
 		placement: 'right',
-		content: () => ({
-			title: __('Modules', 'multivendorx'),
-			description: __('Here you can enable or disable marketplace modules.', 'multivendorx'),
-			nextBtn: {
-				link: `${appLocalizer.site_url}/wp-admin/admin.php?page=multivendorx#&tab=modules`,
-				step: 5
-			}
-		}),
+		title: __('Modules', 'multivendorx'),
+		description: __('Here you can enable or disable marketplace modules.', 'multivendorx'),
+		next: {
+			link: `${appLocalizer.site_url}/wp-admin/admin.php?page=multivendorx#&tab=modules`,
+			step: 5
+		}
 	},
 	{
 		selector: '[data-tour="simple-showcase-tour"]',
 		placement: 'auto',
-		content: () => ({
-			title: __('Modules', 'multivendorx'),
-			description: __('Here you can enable or disable marketplace modules.', 'multivendorx'),
-			nextBtn: {
-				link: `${appLocalizer.site_url}/wp-admin/admin.php?page=multivendorx#&tab=modules`,
-				step: 6
-			}
-		}),
+		title: __('Modules', 'multivendorx'),
+		description: __('Here you can enable or disable marketplace modules.', 'multivendorx'),
+		next: {
+			link: `${appLocalizer.site_url}/wp-admin/admin.php?page=multivendorx#&tab=modules`,
+			step: 6
+		}
 	},
 	{
-		selector: '.category-filter .category-item:nth-child(5) ',
+		selector: '.category-filter .category-item:nth-child(5)',
 		placement: 'auto',
-		content: () => ({
-			title: __('Modules', 'multivendorx'),
-			description: __('Here you can enable or disable marketplace modules.', 'multivendorx'),
-			finishBtn: true
-		}),
-	},
+		title: __('Modules', 'multivendorx'),
+		description: __('Here you can enable or disable marketplace modules.', 'multivendorx'),
+		finish: true
+	}
 ];
