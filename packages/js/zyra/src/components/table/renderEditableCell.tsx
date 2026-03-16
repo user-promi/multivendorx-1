40670877999379237400cf43eec3a51bdd50daeb
Amit Sarkar <88242426+imroxygen@users.noreply.github.com>
2026-03-16 11:44:07 +0530

    fix

--- packages/js/zyra/src/components/table/renderEditableCell.tsx
+++ packages/js/zyra/src/components/table/renderEditableCell.tsx
@@ -1,56 +1,62 @@
 import { TableRow } from './types';
 
 type Props = {
-    header: {};
-    row: TableRow;
-    onSave: (key: string, row: TableRow, value: string|number|boolean) => void;
+    header: any;
+    cell: TableRow;
+    isEditing: boolean;
+    onSave: (value: any) => void;
 };
 
 export const renderEditableCell = ({
-    header,
-    row,
-    onSave,
+	header,
+	cell,
+	isEditing,
+	onSave,
 }: Props) => {
-    const value = row[header.key];
+	const value = cell.value;
 
-    const handleChange = (newValue: string|number|boolean) => {
-        onSave(header.key, row, newValue);
-    };
+	if (!isEditing) {
+		return (
+			<>
+				{cell.display ?? value}
+			</>
+		);
+	}
 
-    switch (header.editType) {
-        case 'toggle':
-            return (
-                <input
-                    type="checkbox"
-                    defaultChecked={Boolean(value)}
-                    onChange={(e) => handleChange(e.target.checked)}
-                    autoFocus
-                />
-            );
+	switch (header.editType) {
+		case 'toggle':
+			return (
+				<input
+					type="checkbox"
+					defaultChecked={Boolean(value)}
+					onChange={(e) => onSave(e.target.checked)}
+					autoFocus
+				/>
+			);
 
-        case 'select':
-            return (
-                <select
-                    defaultValue={String(value)}
-                    onBlur={(e) => handleChange(e.target.value)}
-                    autoFocus
-                >
-                    {header.options?.map((opt: any) => (
-                        <option key={opt.value} value={opt.value}>
-                            {opt.label}
-                        </option>
-                    ))}
-                </select>
-            );
+		case 'select':
+			return (
+				<select
+					defaultValue={String(value)}
+					onBlur={(e) => onSave(e.target.value)}
+					autoFocus
+				>
+					{header.options?.map((opt: any) => (
+						<option key={opt.value} value={opt.value}>
+							{opt.label}
+						</option>
+					))}
+				</select>
+			);
 
-        default:
-            return (
-                <input
-                    type="text"
-                    defaultValue={String(value ?? '')}
-                    onBlur={(e) => handleChange(e.target.value)}
-                    autoFocus
-                />
-            );
-    }
+		default:
+			return (
+				<input
+					type="text"
+					defaultValue={String(value ?? '')}
+					onBlur={(e) => onSave(e.target.value)}
+					autoFocus
+				/>
+			);
+	}
 };
