3a324e4ecc4014fc8bc81820e0866f607005b574
Amit Sarkar <88242426+imroxygen@users.noreply.github.com>
2026-03-27 13:36:18 +0530

    fix

--- plugins/multivendorx/src/components/Settings/Intelligence.ts
+++ plugins/multivendorx/src/components/Settings/Intelligence.ts
@@ -26,6 +26,7 @@ export default {
 				'<a href="https://ai.google.dev/gemini-api/docs/api-key" target="_blank">Click here to generate your Gemini API key.</a>',
 				'multivendorx'
 			),
+			proSetting: true,
 		},
 		// --- OpenAI API Key Section ---
 		{
@@ -40,6 +41,7 @@ export default {
 				'<a href="https://platform.openai.com/api-keys" target="_blank">Click here to generate your OpenAI API key.</a>',
 				'multivendorx'
 			),
+			proSetting: true,
 		},
 		{
 			key: 'openrouter_api_key',
@@ -53,6 +55,7 @@ export default {
 				'<a href="https://openrouter.ai/settings/keys" target="_blank">Generate an OpenRouter API key</a>',
 				'multivendorx'
 			),
+			proSetting: true,
 		},
 		// --- AI Provider Choice ---
 		{
@@ -64,6 +67,7 @@ export default {
 				'multivendorx'
 			),
 			moduleEnabled: 'marketplace-intelligence',
+			proSetting: true,
 		},
 		{
 			key: 'choose_ai_provider',
@@ -98,6 +102,7 @@ export default {
 					icon: 'openrouter',
 				},
 			],
+			proSetting: true,
 		},
 
 		// --- OpenRouter API Key Section ---
@@ -127,6 +132,7 @@ export default {
 					value: 'google/gemini-2.0-flash-exp:free',
 				},
 			],
+			proSetting: true,
 			dependent: {
 				key: 'choose_ai_provider',
 				set: true,
@@ -172,6 +178,7 @@ export default {
 				},
 			],
 			defaultValue: 'gemini_api_image_enhancement',
+			proSetting: true,
 		},
 		// --- Gemini Image Enhancement Settings ---
 		// {
@@ -215,6 +222,7 @@ export default {
 					value: 'google/gemini-3-pro-image-preview',
 				},
 			],
+			proSetting: true,
 			dependent: {
 				key: 'image_enhancement_provider',
 				set: true,
