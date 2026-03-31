1db2c49141a80b87c8b71fb4c2bbd55cf8b411aa
buddhadebroy <roybuddhadeb2003@gmail.com>
2026-03-31 21:07:19 +0530

    tooltip

--- packages/js/zyra/src/components/Popup.tsx
+++ packages/js/zyra/src/components/Popup.tsx
@@ -97,7 +97,7 @@ export const PopupUI = forwardRef<HTMLDivElement, PopupProps>(
                 ref={wrapperRef}
             >
                 {toggleIcon && (
-                    <Tooltip text={tooltipName} position="bottom"  className={open ? 'hidden' : ''}>
+                    <Tooltip text={tooltipName} position="end"  className={open ? 'hidden' : ''}>
                         <i
                             onClick={handleToggle}
                             className={`popup-icon adminfont-${toggleIcon}`}
